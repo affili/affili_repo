3d315c148742f64b71c10b4f72a4a3d9f3450472
murata4455 <murata@mediaxis.jp>
2017-06-09 14:53:44 +0900

    html body １００％

--- media_web/article.php
+++ media_web/article.php
@@ -62,36 +62,59 @@ http://mediaxis.jp/
               □□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□
               □□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□
               □□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□</p>
-              <div id="share">
-<ul>
-<!-- Twitter -->
-<li class="share-twitter">
-<a href="http://twitter.com/home?status=<?php echo urlencode(the_title_attribute('echo=0')); ?>%20<?php the_permalink(); ?>%20by%20" target="_blank">Twitter</a>
-</li>
-<!-- Facebook -->
-<li class="share-facebook">
-<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" target="_blank">Facebook</a>
-</li>
-<!-- Google+ -->
-<li class="share-google">
-<a href="https://plus.google.com/share?url=<?php the_permalink(); ?>" target="_blank">Google+</a>
-</li>
-<!-- はてなブックマーク -->
-<li class="share-hatena">
-<a href="http://b.hatena.ne.jp/add?mode=confirm&url=<?php the_permalink(); ?>&title=<?php the_title();?>" target="_blank">はてブ</a>
-</li>
-<!-- Pocket -->
-<li class="share-pocket">
-<a href="http://getpocket.com/edit?url=<?php the_permalink(); ?>" target="_blank">Pocket</a>
-</li>
-<!-- LINE -->
-<li class="share-line">
-<a href="http://line.me/R/msg/text/?<?php the_title(); ?>%0D%0A<?php the_permalink(); ?>">LINE</a>
-</li>
-</ul>
-</div>
+          <div id="share">
+            <ul>
+              <!-- Twitter -->
+              <li class="share-twitter">
+              <a href="http://twitter.com/home?status=<?php echo urlencode(the_title_attribute('echo=0')); ?>%20<?php the_permalink(); ?>%20by%20" target="_blank">Twitter</a>
+              </li>
+              <!-- Facebook -->
+              <li class="share-facebook">
+              <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" target="_blank">Facebook</a>
+              </li>
+              <!-- Google+ -->
+              <li class="share-google">
+              <a href="https://plus.google.com/share?url=<?php the_permalink(); ?>" target="_blank">Google+</a>
+              </li>
+              <!-- はてなブックマーク -->
+              <li class="share-hatena">
+              <a href="http://b.hatena.ne.jp/add?mode=confirm&url=<?php the_permalink(); ?>&title=<?php the_title();?>" target="_blank">はてブ</a>
+              </li>
+              <!-- Pocket -->
+              <li class="share-pocket">
+              <a href="http://getpocket.com/edit?url=<?php the_permalink(); ?>" target="_blank">Pocket</a>
+              </li>
+              <!-- LINE -->
+              <li class="share-line">
+              <a href="http://line.me/R/msg/text/?<?php the_title(); ?>%0D%0A<?php the_permalink(); ?>">LINE</a>
+              </li>
+            </ul>
+          </div><!--id=share-->
+
+            <div class="back-url">
+              <p><a href="#">＜＜次の記事</a>　<a href="#">前の記事＞＞</a></p>
+            </div>
         </article>
+
+        <div class="ad-wid">
+          <img src="image/ad-wid.png" alt="ad-wid" class="ad-wid-img" />
+        </div>
+
+        <div class="new-contents">
+          <section class="card">
+            <img class="card-img" src="image/newyork.jpg" alt="">
+            <div class="card-content">
+              <h1 class="card-title">New Content</h1>
+              <p class="card-text">ガジェット最新情報</p>
+            </div>
+            <div class="card-link">
+              <a href="http://webcreatorbox.com/about">About</a>
+              <a href="http://webcreatorbox.com/">Website</a>
+            </div>
+          </section>
+        </div><!--new-contents-->
       </div>
+
 
       <div id="article-right">
         <ul>
@@ -126,8 +149,7 @@ http://mediaxis.jp/
         </a>
         </li>
       </ul>
-      </div>
-    </div>
+    </div><!--article-right-->
 
 
   <footer class="footer">
